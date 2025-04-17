(fix) : implementation of Kendall paper for fusion parameters instead of kl div + softmax params

src/torchtmpl/losses.py
@@ -25,8 +25,8 @@ class NTXentLoss(nn.Module):
         norm = torch.linalg.norm(z, dim=1, keepdim=True).clamp_min(self.eps)
         z_normalized = z / norm  # shape (2N, D)
 
-        # similarity with real part of hermitian product 
-        sim = torch.matmul(z_normalized, z_normalized.conj().T).real  # (2N, 2N)
+        # similarity with module of hermitian products
+        sim = torch.abs(torch.matmul(z_normalized, z_normalized.conj().T))  # (2N, 2N)
         # clamp to avoid overflow
         sim = sim.clamp(-1 + self.eps, 1 - self.eps)
 

src/torchtmpl/utils/training_contrastive_utils.py
@@ -41,17 +41,11 @@ def train_one_contrastive_epoch(
         losses = torch.stack([f_loss(z1, z2) for z1, z2 in zip(zs1, zs2)], dim=0)
 
         # Learnable fusion of losses (again, instead of defining weights in config)
-
-        u = torch.real(model.loss_weights)
-        w = F.softmax(u, dim=0)     # w_i >= 0, sum = 1
-        contrastive_loss = (w * losses).sum()
-
-        # KL-divergence vers l’uniforme u = 1/N
-        # formula  = KL(w || uniform) = sum_i w_i * log(w_i * N)
-        kl = torch.sum(w * torch.log(w * N + 1e-12))
-
-        # final loss
-        loss = contrastive_loss + lambda_kl * kl
+        precision = torch.exp(-model.log_vars)   # 1/σ_i²
+        contrastive_loss = torch.sum(precision * losses)
+        
+        # régularisation log σ_i (cf. Kendall & Gal, CVPR 2018)
+        loss = contrastive_loss + torch.sum(model.log_vars)
 
         # backward + clipping
         optim.zero_grad()
@@ -69,13 +63,7 @@ def train_one_contrastive_epoch(
         optim.step()
 
         with torch.no_grad():
-            rw = model.loss_weights.data.real
-            iw = model.loss_weights.data.imag
-
-            rw_clamped = rw.clamp(-10.0, 10.0)
-            iw_clamped = iw.clamp(-10.0, 10.0)
-
-            model.loss_weights.data = torch.complex(rw_clamped, iw_clamped)
+            model.log_vars.data.clamp_(-5.0, 5.0) 
 
         # step scheduler
         if isinstance(scheduler, (torch.optim.lr_scheduler.CyclicLR,